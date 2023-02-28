8b6fd5c7cc037bc0876e6ef9b20ae8fa349a1133
jiwoob <114009384+jiwoob@users.noreply.github.com>
2023-02-28 11:47:54 +0900

    [D3] Title: 삼성시의 버스 노선, Time: 133 ms, Memory: 22,248 KB -BaekjoonHub

--- SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.java
+++ SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.java
@@ -4,7 +4,6 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-//6485
 public class Solution {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
